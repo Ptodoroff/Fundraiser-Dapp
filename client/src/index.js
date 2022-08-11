7af70510facf55dab2836b91c049ed9c9711621c
Petar Todorov <petartodorov@Petars-MacBook-Pro.local>
2022-08-11 17:40:24 +0300

    finalisation

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-import {BrowserRouter} from 'react-router-dom';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+ReactDOM.render((
+  // <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  // </Provider>
+), document.getElementById('root'))
